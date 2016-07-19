3697c1eee354fed0e665d487e2a5daaa57a866e1
Channing Pear <PearSquirrel@users.noreply.github.com>
2016-07-19 01:50:34 -0700

    changed _min, _max, _range to min_, max_, range_

--- smallest-spanning-range/solution.py
+++ smallest-spanning-range/solution.py
@@ -20,36 +20,36 @@ def min_range(lists):
 
     # initialize tracked values
     min_range = 100000000
-    _max = Node(-1, 0, 0)
+    max_ = Node(-1, 0, 0)
 
     # populate a min-heap with the lists' left-most elements and track the max
     h = []
     for _list in lists:
         heappush(h, _list[0])
-        if _list[0].value > _max.value:
-            _max = _list[0]
+        if _list[0].value > max_.value:
+            max_ = _list[0]
 
     # track the current minimum range, stopping once a list has been depleted
     while True:
         # grab the minimum from the heap
-        _min = heappop(h)
-        _range = _max.value - _min.value
+        min_ = heappop(h)
+        range_ = max_.value - min_.value
 
         # update the min_range if needed
-        if _range < min_range:
-            min_range = _range
-            min_l = _min.value
-            min_r = _max.value
+        if range_ < min_range:
+            min_range = range_
+            min_l = min_.value
+            min_r = max_.value
 
         # if a list has been processed, the smallest range has been found
-        if _min.index >= len(lists[_min.list_index]) - 1:
+        if min_.index >= len(lists[min_.list_index]) - 1:
             return (min_l, min_r)
 
         # if there exists a neighbor, add it to the heap
-        neighbor = lists[_min.list_index][_min.index + 1]
+        neighbor = lists[min_.list_index][min_.index + 1]
         heappush(h, neighbor)
-        if neighbor.value > _max.value:
-            _max = neighbor
+        if neighbor.value > max_.value:
+            max_ = neighbor
 
 # main program
 _input = [[4, 10, 15, 24, 26], [0, 9, 12, 20], [5, 18, 22, 30]]
