d485f4b53748bcc09f0b82f895f3b92939ed5830
Channing Pear <PearSquirrel@users.noreply.github.com>
2016-07-29 00:20:23 -0700

    swapped linear search for binary search

--- longest-increasing-subsequence/solution.py
+++ longest-increasing-subsequence/solution.py
@@ -13,36 +13,36 @@ def ll_to_list(node):
         node = node.next_
     return list_
 
+def binary_search(list_, element):
+    lo = 0
+    hi = len(list_)
+    while lo < hi:
+        mid = lo + (hi - lo) // 2
+        if element < list_[mid]:
+            hi = mid
+        else: # element <= list_[mid]
+            lo = mid + 1
+    return lo - 1
+
 def longest_increasing_subsequence(list_):
     heads = []
-    # TODO: replace linear search with a binary search
-    """
-        lo = 0
-        hi = len(list_)
-        while hi > lo:
-            mid = lo + (hi - lo) // 2
-            if list_[mid] == element:
-                return mid
-            if list_[mid] < element:
-                lo = mid + 1
-            else: # list_[mid] > element
-                hi = mid
-        return -1
-    """
+    for el in list_:
+        # get longest list with head < el
+        index = binary_search([head.val for head in heads], el)
+        # get the next node
+        if index == -1: # subsequence length == 1
+            next_ = Node(el)
+        else: # 0 <= index < len(heads)
+            next_ = Node(el, heads[index])
 
-    for i, el in enumerate(list_):
-        # get longest list with head < list_[i]
-        index = 0
-        while index < len(heads) and el >= heads[index].val:
-            index += 1
-        if index >= len(heads):
-            heads.append([])
-        if index == 0:
-            heads[index] = Node(el)
+        # if we've found a longer subsequence, append it to our list
+        if index == len(heads) - 1:
+            heads.append(next_)
+        # otherwise update the existing subsequence with the same length
         else:
-            heads[index] = Node(el, heads[index-1])
-
-    return ll_to_list(heads[len(heads)-1])
+            heads[index + 1]  = next_
+        print [ll_to_list(head) for head in heads]
+    return ll_to_list(heads[-1])
 
 list_ = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
 print "list:", list_
